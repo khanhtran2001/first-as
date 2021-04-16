00fd277353714839d62c676303debd7f37775c4f
Tuan Tai <tuantaibk1999@gmail.com>
2021-04-16 15:12:15 +0700

    Creat 2 folders "auto_anotation" and "auto_anotation_json"

--- auto.py
+++ auto.py
@@ -7,3 +7,9 @@ import base64
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 image_dir = "face_data"
+
+if os.path.exists("auto_anotation") == 0:
+    os.makedirs("auto_anotation")
+    
+if os.path.exists("auto_anotation_json") == 0:
+    os.makedirs("auto_anotation_json")
